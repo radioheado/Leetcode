9e3e7f1ec37278252f120626a75cc5557d626fbd
Radio Head <42974719+radioheado@users.noreply.github.com>
2021-12-03 18:04:14 -0500

    Add Q116 & Q117 Solutions
    Using Queue and BFS.

---
+++ Java/116. Populating Next Right Pointers in Each Node.java
@@ -0,0 +1,55 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        
+        if(root == null){
+            return root;
+        }
+        
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.add(root);
+        
+        while(!queue.isEmpty()){
+            
+            int size = queue.size();
+            for(int i=0; i<size; i++){
+                Node node = queue.poll();
+                if(i<size-1){
+                    node.next = queue.peek();
+                }
+                
+                // add its children if they exist
+                // only performing one check condition because the tree is perfect
+                if(node.left != null){
+                    queue.add(node.left);
+                    queue.add(node.right);
+                }
+            }
+        }
+        
+        
+        return root;
+    }
+}
