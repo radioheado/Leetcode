18ba67cea9d9c741309392dc744a7d23fa348b93
Radio Head <42974719+radioheado@users.noreply.github.com>
2021-12-27 17:31:44 -0500

    Update 88. Merge Sorted Array.py

--- Python3/88. Merge Sorted Array.py
+++ Python3/88. Merge Sorted Array.py
@@ -3,20 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        l = m-1
-        r = n-1
-        p = m+n-1
+        i, j = m-1, n-1
         
-        while l>=0 and r>=0:
-            if nums1[l] >= nums2[r]:
-                nums1[p] = nums1[l]
-                l -= 1
-            else:
-                nums1[p] = nums2[r]
-                r -= 1
-            p -= 1
+        for k in range(m+n-1, -1, -1):
+            if i >= 0 and j >= 0:
+                if nums1[i] >= nums2[j]:
+                    nums1[k] = nums1[i]
+                    i -= 1
+                else:
+                    nums1[k] = nums2[j]
+                    j -= 1
             
-        while r>= 0:
-            nums1[p] = nums2[r]
-            r -= 1
-            p -= 1
+            # be careful of the case that nums2 has not been added to nums1 completely
+            # which means there are elements in nums2 that are smaller than the first element in nums1
+            # thus, j remains larger than 0
+            # we don't need to do this for nums1 when i >= 0
+            # that's because those elements are already in nums1
+            elif j >= 0:
+                nums1[k] = nums2[j]
+                j -= 1
