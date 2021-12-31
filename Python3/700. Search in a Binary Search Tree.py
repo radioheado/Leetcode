eba02c9f77ab41da7192b0f530f94a0ace266bd5
Radio Head <42974719+radioheado@users.noreply.github.com>
2021-12-31 16:30:39 -0500

    Add Q700 Solution

---
+++ Python3/700. Search in a Binary Search Tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# Recursive Solution
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        
+        if not root:
+            return
+        
+        if root.val > val:
+            return self.searchBST(root.left, val)
+        
+        elif root.val < val:
+            return self.searchBST(root.right, val)
+        
+        else:
+            return root
