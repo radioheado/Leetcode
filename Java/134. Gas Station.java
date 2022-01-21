0fa033dc83fe55c064f2977e0cecc092a57f58e4
Radio Head <42974719+radioheado@users.noreply.github.com>
2022-01-21 16:40:59 -0500

    Add Q134 Solution

---
+++ Java/134. Gas Station.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int total = 0, cur = 0, start = 0;
+        
+        for(int i=0; i<gas.length; i++){
+            total += gas[i]-cost[i];
+            cur   += gas[i]-cost[i];
+            
+            if(cur < 0){
+                start = i+1;
+                cur = 0;
+            }
+        }
+        
+        return total >= 0 ? start : -1;
+    }
+}
