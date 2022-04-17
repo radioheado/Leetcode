272e765fe628d66ef3d821a133ac452cb7dd1f23
Radio Head <jhuang21@gmu.edu>
2022-04-17 15:20:49 -0400

    Add Q538 Solution

---
+++ Python3/538. Convert BST to Greater Tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        vals = []
+        self.total = 0
+        
+        def dfs(node = root):
+            if not node:
+                return
+            
+            dfs(node.right)
+            self.total += node.val
+            node.val = self.total
+            dfs(node.left)
+            
+        dfs()
+        return root
+        
+        
