01f1fecda34835fbf411976411c85242d8ee7074
Radio Head <jhuang21@gmu.edu>
2022-10-24 16:27:52 -0400

    Add Q2095 Solution

---
+++ Java/2095. Delete the Middle Node of a Linked List.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if (head.next == null) {
+            return null;
+        }
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode fast = dummy, slow = dummy;
+        
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
