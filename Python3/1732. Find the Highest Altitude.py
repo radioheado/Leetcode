71a41530ebc16254ac8be7712a792799aabdafb6
Radio Head <42974719+radioheado@users.noreply.github.com>
2023-06-19 15:24:13 -0400

    Add Q1732 Solution

---
+++ Python3/1732. Find the Highest Altitude.py
@@ -0,0 +1,9 @@
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        ans = alt = 0
+
+        for g in gain:
+            alt += g
+            ans = max(ans, alt)
+
+        return ans
