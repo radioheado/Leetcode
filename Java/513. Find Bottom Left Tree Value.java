ac70f93679b9f77dc4919a6d5fe37e4ed79b2aab
JJ <42974719+radioheado@users.noreply.github.com>
2024-02-28 10:12:50 -0500

    Add Q513 Solution

---
+++ Java/513. Find Bottom Left Tree Value.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        int ans = root.val;
+
+        while (!q.isEmpty()) {
+            int level = q.size();
+            ans = q.peek().val;
+
+            for (int i = 0; i < level; i++) {
+                root = q.poll();
+                if (root.left != null) {
+                    q.offer(root.left);
+                }
+                if (root.right != null) {
+                    q.offer(root.right);
+                }
+            }
+        }
+
+        return ans;
+    }
+}
---
+++ Python3/513. Find Bottom Left Tree Value.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return root
+
+        ans = None
+        q = deque([root])
+
+        while q:
+            level = len(q)
+            ans = q[0].val
+            for _ in range(level):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+        return ans
