96c5cba292339d09591d26dd8595adc29f2b673c
JJ <42974719+radioheado@users.noreply.github.com>
2024-03-01 16:44:07 -0500

    Add Q2864 Solution

---
+++ Java/2864. Maximum Odd Binary Number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String maximumOddBinaryNumber(String s) {
+        int ones = 0, size = s.length();
+        for (char c: s.toCharArray()) {
+            if (c == '1') {
+                ones++;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < ones - 1; i++) {
+            sb.append('1');
+        }
+        for (int i = 0; i < size - ones; i++) {
+            sb.append('0');
+        }
+        sb.append('1');
+        return sb.toString();
+    }
+}
