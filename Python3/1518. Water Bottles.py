480ebfc51131af795bdb10ae200d4602d99adb9b
JJ <42974719+radioheado@users.noreply.github.com>
2024-07-07 13:09:04 -0400

    Add Q1518 Solution

---
+++ Python3/1518. Water Bottles.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        ans = empty = 0
+        while numBottles:
+            ans += numBottles
+            numBottles, empty = divmod(numBottles + empty, numExchange)
+            
+        return ans
