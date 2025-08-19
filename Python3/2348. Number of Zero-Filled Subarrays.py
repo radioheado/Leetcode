56544c6eaf765d77b212a59637900103833a89ae
JJ <42974719+radioheado@users.noreply.github.com>
2025-08-19 14:21:21 -0400

    Updated Q2348 Python Solution

--- Python3/2348. Number of Zero-Filled Subarrays.py
+++ Python3/2348. Number of Zero-Filled Subarrays.py
@@ -1,22 +1,12 @@
 class Solution:
     def zeroFilledSubarray(self, nums: List[int]) -> int:
-        start = -1
-        ans = 0
-
-        for i, n in enumerate(nums):
-            if n != 0:
-                # we have marked a start
-                if start != -1:
-                    l = i - start
-                    ans += (l+1) * l // 2
-                start = -1
+        ans = zeroes = 0
+        for num in nums:
+            if num == 0:
+                zeroes += 1
             else:
-                if start == -1:
-                    start = i
+                zeroes = 0
 
-        # we have a tail with 0's
-        if start != -1:
-            l = len(nums) - start
-            ans += (l+1) * l // 2
-
+            ans += zeroes
+        
         return ans
