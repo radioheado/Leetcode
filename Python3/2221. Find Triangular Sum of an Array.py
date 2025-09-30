ac1aefbb34ac6a492ef178cc8ecc516ed5ce1504
JJ <42974719+radioheado@users.noreply.github.com>
2025-09-30 12:51:06 -0400

    Add Q2221 Solution

---
+++ Python3/2221. Find Triangular Sum of an Array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def triangularSum(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        n = len(nums)
+
+        while n > 1:
+            for i in range(n - 1):
+                nums[i] = (nums[i] + nums[i + 1]) % 10
+            n -= 1
+
+        return nums[0]
